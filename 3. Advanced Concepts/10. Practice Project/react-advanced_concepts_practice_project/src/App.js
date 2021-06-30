4d727e6abe906301b9ba1355f8cddd51709d5d47
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-06-30 22:08:07 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 10. Practice Project (Adding a 'Header' Component).

--- 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/App.js
+++ 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
 
 function App() {
    return (
-      <div>
-         <h2>Food Ordering App</h2>
-      </div>
+      <Fragment>
+         <Header />
+      </Fragment>
    );
 };
 
