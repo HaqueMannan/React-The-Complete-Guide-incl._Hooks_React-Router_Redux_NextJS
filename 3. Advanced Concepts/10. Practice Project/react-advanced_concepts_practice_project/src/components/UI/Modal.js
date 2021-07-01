13c4e70b7447bda640f7845c43f0d1f2c107886b
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-01 23:08:06 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 10. Practice Project (Managing Cart & Modal State).

--- 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/components/UI/Modal.js
+++ 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/components/UI/Modal.js
@@ -5,7 +5,7 @@ import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
    return(
-      <div className={classes.backdrop}></div>
+      <div className={classes.backdrop} onClick={props.onClose}></div>
    );
 };
 
@@ -23,7 +23,7 @@ const Modal = (props) => {
    return(
       <Fragment>
          {ReactDOM.createPortal(
-            <Backdrop />,
+            <Backdrop onClose={props.onClose} />,
             portalElement
          )}
          {ReactDOM.createPortal(
