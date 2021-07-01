c7ca8c8092513a007330f699cd93d521e2dc9e75
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-01 22:48:46 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 10. Practice Project (Adding a Modal via a React Portal).

--- 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/components/Cart/Cart.js
+++ 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/components/Cart/Cart.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import classes from './Cart.module.css';
 
+import Modal from '../UI/Modal';
+
 const Cart = (props) => {
    const cartItems = (
       <ul className={classes['cart-items']}>
@@ -12,7 +14,7 @@ const Cart = (props) => {
    );
 
    return(
-      <div>
+      <Modal>
          {cartItems}
          <div className={classes.total}>
             <span>Total Amount</span>
@@ -22,7 +24,7 @@ const Cart = (props) => {
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
          </div>
-      </div>
+      </Modal>
    );
 };
 
