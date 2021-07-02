e45b5cead84b763db0d0ccd1420ee7be7f29c4fb
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-03 00:29:09 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 10. Practice Project (Working with Refs & Forward Refs).

--- 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/store/CartProvider.js
+++ 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/store/CartProvider.js
@@ -8,14 +8,16 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-   if(action.type === ' ADD') {
+   if(action.type === 'ADD') {
       const updatedItems = state.items.concat(action.item);
       const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
       return {
          items: updatedItems,
          totalAmount: updatedTotalAmount
       };
    };
+
    return defaultCartState;
 };
 
