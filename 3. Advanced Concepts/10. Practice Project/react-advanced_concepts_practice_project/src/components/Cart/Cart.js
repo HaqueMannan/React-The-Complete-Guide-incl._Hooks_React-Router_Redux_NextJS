9c65258fc6487add8ba558ecf3377bea98a5950a
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-03 16:13:36 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 10. Practice Project (Making Cart Items Removable from the Cart Component).

--- 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/components/Cart/Cart.js
+++ 3. Advanced Concepts/10. Practice Project/react-advanced_concepts_practice_project/src/components/Cart/Cart.js
@@ -9,16 +9,21 @@ import CartContext from '../../store/cart-context';
 const Cart = (props) => {
    const cartCtx = useContext(CartContext);
 
-   const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}`;
+   const totalAmount = `£${Math.abs(cartCtx.totalAmount).toFixed(2)}`;
    const hasItems = cartCtx.items.length > 0;
 
-   const cartItemRemoveHandler = (id) => {};
-   const cartItemAddHandler = (item) => {};
+   const cartItemRemoveHandler = (id) => {
+      cartCtx.removeItem(id);
+   };
+
+   const cartItemAddHandler = (item) => {
+      cartCtx.addItem({...item, amount: 1});
+   };
 
    const cartItems = (
       <ul className={classes['cart-items']}>
          {cartCtx.items.map((item) => (
-            <CartItem 
+            <CartItem
                key={item.id}
                name={item.name}
                amount={item.amount}
