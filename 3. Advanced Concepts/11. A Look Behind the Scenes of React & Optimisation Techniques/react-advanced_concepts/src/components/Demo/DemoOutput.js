ef8eafb745a8803d2a1f342892022fe20a2d9d6a
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-04 18:01:48 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 11. A Look Behind the Scenes of React & Optimisation Techniques (A Closer Look at Child Component Re-Evaluation).

---
+++ 3. Advanced Concepts/11. A Look Behind the Scenes of React & Optimisation Techniques/react-advanced_concepts/src/components/Demo/DemoOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import MyParagraph from './MyParagraph';
+
+const DemoOutput = (props) => {
+   console.log('DemoOutput RUNNING');
+   return(
+      <MyParagraph>{props.show ? 'This paragraph is new!' : ''}</MyParagraph>
+   );
+};
+
+export default DemoOutput;
