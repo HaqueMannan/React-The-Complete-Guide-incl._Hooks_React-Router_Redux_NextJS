63d49f77f4dd7f0420a0bee7e784d12ad2bf2a5a
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-10 20:22:53 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 12. An Alternative Way of Building Components: Class-based Components (Class-based Components and Context).

--- 3. Advanced Concepts/12. An Alternative Way of Building Components  (Class-based Components)/react-advanced_concepts/src/App.js
+++ 3. Advanced Concepts/12. An Alternative Way of Building Components  (Class-based Components)/react-advanced_concepts/src/App.js
@@ -1,11 +1,23 @@
 import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+   { id: 'u1', name: 'Alex' },
+   { id: 'u2', name: 'Beth' },
+   { id: 'u3', name: 'Charlie' },
+   { id: 'u4', name: 'Diane' }
+];
 
 function App() {
+   const usersContext = {
+      users: DUMMY_USERS
+   }
+
    return (
-      <div>
+      <UsersContext.Provider value={usersContext}>
          <UserFinder />
-      </div>
+      </UsersContext.Provider>
    );
-}
+};
 
 export default App;
--- 3. Advanced Concepts/12. An Alternative Way of Building Components  (Class-based Components)/react-advanced_concepts/src/components/UserFinder.js
+++ 3. Advanced Concepts/12. An Alternative Way of Building Components  (Class-based Components)/react-advanced_concepts/src/components/UserFinder.js
@@ -1,15 +1,11 @@
 import { Fragment, Component } from 'react';
 import Users from './Users';
 import classes from './UserFinder.module.css';
-
-const DUMMY_USERS = [
-   { id: 'u1', name: 'Alex' },
-   { id: 'u2', name: 'Beth' },
-   { id: 'u3', name: 'Charlie' },
-   { id: 'u4', name: 'Diane' }
-];
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
+   static contextType = UsersContext;
+
    constructor() {
       super();
       this.state = {
@@ -25,14 +21,14 @@ class UserFinder extends Component {
    componentDidMount() {
       // Send http request...
       this.setState({
-         filteredUsers: DUMMY_USERS
+         filteredUsers: this.context.users
       });
    };
 
    componentDidUpdate(prevProps, prevState) {
       if(prevState.searchTerm !== this.state.searchTerm) {
          this.setState({
-            filteredUsers: DUMMY_USERS.filter((user) => 
+            filteredUsers: this.context.users.filter((user) => 
                user.name.includes(this.state.searchTerm)
             )
          });
@@ -48,7 +44,7 @@ class UserFinder extends Component {
             <Users users={this.state.filteredUsers} />
          </Fragment>
       );
-   }
+   };
 };
 
 export default UserFinder;
