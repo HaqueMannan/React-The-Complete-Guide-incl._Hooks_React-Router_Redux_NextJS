e1eb2fb90abaa234daea5bd8aad898f9c7fa6f89
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-11 12:31:40 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 13. Sending HTTP Requests: Connecting a Database (Handling Loading & Data States).

--- 3. Advanced Concepts/13. Sending HTTP Requests (Connecting a Database)/react-advanced_concepts/src/App.js
+++ 3. Advanced Concepts/13. Sending HTTP Requests (Connecting a Database)/react-advanced_concepts/src/App.js
@@ -4,24 +4,11 @@ import MoviesList from './components/MoviesList';
 
 function App() {
    const [movies, setMovies] = useState([]);
-
-   // function fetchMoviesHandler() {
-   //    fetch('https://swapi.dev/api/films/').then((response) => {
-   //       return response.json();
-   //    }).then((data) => {
-   //       const transformedMovies = data.results.map((movieData) => {
-   //          return {
-   //             id: movieData.episode_id,
-   //             title: movieData.title,
-   //             openingText: movieData.opening_crawl,
-   //             releaseDate: movieData.release_date 
-   //          };
-   //       });
-   //       setMovies(transformedMovies);
-   //    });
-   // };
+   const [isLoading, setIsLoading] = useState(false);
 
    async function fetchMoviesHandler() {
+      setIsLoading(true);
+
       const response = await fetch('https://swapi.dev/api/films/')
       const data = await response.json();
 
@@ -35,6 +22,7 @@ function App() {
       });
 
       setMovies(transformedMovies);
+      setIsLoading(false);
    };
 
    return (
@@ -43,7 +31,9 @@ function App() {
             <button onClick={fetchMoviesHandler}>Fetch Movies</button>
          </section>
          <section>
-            <MoviesList movies={movies} />
+            {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+            {!isLoading && movies.length === 0 && <p>No Movies Found.</p>}
+            {isLoading && <p>Loading...</p>}
          </section>
       </React.Fragment>
    );
