f34a5d4cea17405d7f9f4efa444fce87e15578b6
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-11 13:19:34 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 13. Sending HTTP Requests: Connecting a Database (Handling HTTP Errors).

--- 3. Advanced Concepts/13. Sending HTTP Requests (Connecting a Database)/react-advanced_concepts/src/App.js
+++ 3. Advanced Concepts/13. Sending HTTP Requests (Connecting a Database)/react-advanced_concepts/src/App.js
@@ -5,25 +5,43 @@ import MoviesList from './components/MoviesList';
 function App() {
    const [movies, setMovies] = useState([]);
    const [isLoading, setIsLoading] = useState(false);
+   const [error, setError] = useState(null);
 
    async function fetchMoviesHandler() {
       setIsLoading(true);
+      setError(null);
 
-      const response = await fetch('https://swapi.dev/api/films/')
-      const data = await response.json();
+      try {
+         // const response = await fetch('https://swapi.dev/api/film/');   // Trigger error in HTTP request.
+         const response = await fetch('https://swapi.dev/api/films/');
 
-      const transformedMovies = data.results.map((movieData) => {
-         return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date 
+         if(!response.ok) {
+            throw new Error('Something went wrong');
          };
-      });
 
-      setMovies(transformedMovies);
+         const data = await response.json();
+
+         const transformedMovies = data.results.map((movieData) => {
+            return {
+               id: movieData.episode_id,
+               title: movieData.title,
+               openingText: movieData.opening_crawl,
+               releaseDate: movieData.release_date 
+            };
+         });
+
+         setMovies(transformedMovies);
+         setIsLoading(false);
+      } catch(error) {
+         setError(error.message);
+      };
       setIsLoading(false);
    };
+
+   let content = <p>No Movies Found.</p>;
+   if(movies.length > 0) { content = <MoviesList movies={movies} /> };
+   if(error) { content = <p>{error}</p>; };
+   if(isLoading) { content = <p>Loading...</p>; };
 
    return (
       <React.Fragment>
@@ -31,9 +49,7 @@ function App() {
             <button onClick={fetchMoviesHandler}>Fetch Movies</button>
          </section>
          <section>
-            {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-            {!isLoading && movies.length === 0 && <p>No Movies Found.</p>}
-            {isLoading && <p>Loading...</p>}
+            {content}
          </section>
       </React.Fragment>
    );
