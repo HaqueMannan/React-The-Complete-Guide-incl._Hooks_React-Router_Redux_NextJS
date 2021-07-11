8da1c1759f17f7e2fcab1aa3eda739e83abdc654
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-11 13:56:15 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 13. Sending HTTP Requests: Connecting a Database (Using the useEffect() Hook for Requests).

--- 3. Advanced Concepts/13. Sending HTTP Requests (Connecting a Database)/react-advanced_concepts/src/App.js
+++ 3. Advanced Concepts/13. Sending HTTP Requests (Connecting a Database)/react-advanced_concepts/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import MoviesList from './components/MoviesList';
 
@@ -7,7 +7,7 @@ function App() {
    const [isLoading, setIsLoading] = useState(false);
    const [error, setError] = useState(null);
 
-   async function fetchMoviesHandler() {
+   const fetchMoviesHandler = useCallback(async () => {
       setIsLoading(true);
       setError(null);
 
@@ -36,7 +36,11 @@ function App() {
          setError(error.message);
       };
       setIsLoading(false);
-   };
+   }, []);
+
+   useEffect(() => {
+      fetchMoviesHandler();
+   }, [fetchMoviesHandler]);
 
    let content = <p>No Movies Found.</p>;
    if(movies.length > 0) { content = <MoviesList movies={movies} /> };
