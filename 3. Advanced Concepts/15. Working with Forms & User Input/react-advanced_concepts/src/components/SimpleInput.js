00e1d7ab213e7e34238d44f57539519af5a80bc9
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-24 14:45:53 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 15. Working with Forms & User Input (Dealing With Form Submission & Getting User Input Values).

--- 3. Advanced Concepts/15. Working with Forms & User Input/react-advanced_concepts/src/components/SimpleInput.js
+++ 3. Advanced Concepts/15. Working with Forms & User Input/react-advanced_concepts/src/components/SimpleInput.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
+   // Read User Input with useRef:
+   const nameInputRef = useRef();
+
+   // Read User Input On Every Keystroke:
+   const [enteredName, setEnteredName] = useState('');
+   const nameInputChangeHandler = (event) => {
+      setEnteredName(event.target.value);
+   };
+
+   const formSubmissionHandler = (event) => {
+      event.preventDefault();
+
+      // Value from useState:
+      console.log(enteredName);
+
+      // Value from useRef:
+      const enteredValue = nameInputRef.current.value;
+      console.log(enteredValue);
+
+      // Reset input using useState and the value prop:
+      setEnteredName('');
+
+      // Reset input using useRef:
+      // nameInputRef.current.value = ''     // NOT RECOMMENDED, DO NOT MANIPULATE THE REAL DOM DIRECTLY, LET REACT MANIPULATE THE DOM.
+   };
+
    return (
-      <form>
+      <form onSubmit={formSubmissionHandler} >
          <div className='form-control'>
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' />
+            <input type='text' id='name' ref={nameInputRef} onChange={nameInputChangeHandler} value={enteredName} />
          </div>
 
          <div className="form-actions">
