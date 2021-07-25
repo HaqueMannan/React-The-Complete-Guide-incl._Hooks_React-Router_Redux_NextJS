8f9f7a4131f382922d5a6730881ceae3c7c0afb1
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-25 17:32:27 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 15. Working with Forms & User Input (Managing the Overall Form Validity).

--- 3. Advanced Concepts/15. Working with Forms & User Input/react-advanced_concepts/src/components/SimpleInput.js
+++ 3. Advanced Concepts/15. Working with Forms & User Input/react-advanced_concepts/src/components/SimpleInput.js
@@ -7,6 +7,21 @@ const SimpleInput = (props) => {
    const enteredNameIsValid = enteredName.trim() !== ''
    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+   // Overall Form Validity with useEffect:
+   // const [formIsValid, setFormIsValid] = useState(false);
+   // useEffect(() => {
+   //    if(enteredNameIsValid) {
+   //       setFormIsValid(true);
+   //    } else {
+   //       setFormIsValid(false);
+   //    };
+   // }, [enteredNameIsValid]);
+
+   let formIsValid = false;
+   if(enteredNameIsValid) {
+      formIsValid = true;
+   };
+
    const nameInputChangeHandler = (event) => {
       setEnteredName(event.target.value);
    };
@@ -39,7 +54,7 @@ const SimpleInput = (props) => {
          </div>
 
          <div className="form-actions">
-            <button>Submit</button>
+            <button disabled={!formIsValid}>Submit</button>
          </div>
       </form>
    );
