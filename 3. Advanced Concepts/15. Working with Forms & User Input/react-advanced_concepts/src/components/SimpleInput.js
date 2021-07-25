26688f8021443fab90ec1ec0c562b5ceb42611d0
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-07-25 16:45:28 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 15. Working with Forms & User Input (Refactoring & Deriving States).

--- 3. Advanced Concepts/15. Working with Forms & User Input/react-advanced_concepts/src/components/SimpleInput.js
+++ 3. Advanced Concepts/15. Working with Forms & User Input/react-advanced_concepts/src/components/SimpleInput.js
@@ -1,68 +1,40 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const SimpleInput = (props) => {
-   // Read User Input with useRef:
-   const nameInputRef = useRef();
-
-   // Read User Input On Every Keystroke:
    const [enteredName, setEnteredName] = useState('');
+   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+   const enteredNameIsValid = enteredName.trim() !== ''
+   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
    const nameInputChangeHandler = (event) => {
       setEnteredName(event.target.value);
    };
 
-   // Form Validation Example:
-   const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
-   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-
-   useEffect(() => {
-      if(enteredNameIsValid) {
-         console.log('Name input is valid!');
-      };
-   }, [enteredNameIsValid]);
-
    const nameInputBlurHandler = (event) => {
       setEnteredNameTouched(true);
-
-      if(enteredName.trim() === '') {
-         setEnteredNameIsValid(false);
-         return;
-      };
    };
 
    const formSubmissionHandler = (event) => {
       event.preventDefault();
 
-      setEnteredNameTouched(true);
-
-      if(enteredName.trim() === '') {
-         setEnteredNameIsValid(false);
+      if(!enteredNameIsValid) {
          return;
       };
 
-      setEnteredNameIsValid(true);
-
-      // Value from useState:
       console.log(enteredName);
 
-      // Value from useRef:
-      const enteredValue = nameInputRef.current.value;
-      console.log(enteredValue);
-
-      // Reset input using useState and the value prop:
       setEnteredName('');
-
-      // Reset input using useRef:
-      // nameInputRef.current.value = ''     // NOT RECOMMENDED, DO NOT MANIPULATE THE REAL DOM DIRECTLY, LET REACT MANIPULATE THE DOM.
+      setEnteredNameTouched(false);
    };
 
-   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
    const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
    return(
       <form onSubmit={formSubmissionHandler} >
          <div className={nameInputClasses} >
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' ref={nameInputRef} onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName} />
+            <input type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName} />
             { nameInputIsInvalid && <p className="error-text">Name must not be empty.</p> }
          </div>
 
