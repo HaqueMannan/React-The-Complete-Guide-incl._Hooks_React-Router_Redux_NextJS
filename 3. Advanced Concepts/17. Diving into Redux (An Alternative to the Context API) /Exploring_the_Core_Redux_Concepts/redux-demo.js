28ff720b5887604c236425c131de8ad17e0bc846
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-08-07 22:49:20 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 17. Diving into Redux: An Alternative to the Context API (More Redux Basics).

--- 3. Advanced Concepts/17. Diving into Redux (An Alternative to the Context API) /Exploring_the_Core_Redux_Concepts/redux-demo.js
+++ 3. Advanced Concepts/17. Diving into Redux (An Alternative to the Context API) /Exploring_the_Core_Redux_Concepts/redux-demo.js
@@ -1,9 +1,19 @@
 const redux = require('redux');
 
 const counterFunction = (state = { counter: 0 }, action) => {
-   return {
-      counter: state.counter + 1
+   if(action.type === 'increment') {
+      return {
+         counter: state.counter + 1
+      };
    };
+
+   if(action.type === 'decrement') {
+      return {
+         counter: state.counter - 1
+      };
+   };
+
+   return state;
 };
 
 const store = redux.createStore(counterFunction);
@@ -17,3 +27,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
