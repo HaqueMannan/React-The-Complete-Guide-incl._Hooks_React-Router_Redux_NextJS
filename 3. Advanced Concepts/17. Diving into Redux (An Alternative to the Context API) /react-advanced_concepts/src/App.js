0bf1cbb1bc2d55bd7858d29d81a35e8a1c7dbea0
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-08-10 21:58:18 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 17. Diving into Redux: An Alternative to the Context API (Using Redux with Class-based Components).

--- 3. Advanced Concepts/17. Diving into Redux (An Alternative to the Context API) /react-advanced_concepts/src/App.js
+++ 3. Advanced Concepts/17. Diving into Redux (An Alternative to the Context API) /react-advanced_concepts/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Counter from './components/Counter';
+// import Counter from './components/Counter-ClassBasedComponent';
 
 function App() {
    return (
-      <Counter />
+      <div>
+         <Counter />
+      </div>
    );
 };
 
