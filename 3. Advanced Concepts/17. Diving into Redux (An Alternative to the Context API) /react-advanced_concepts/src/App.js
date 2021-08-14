292cfb326052780ad5a00f0f8dad490d7743ee5f
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-08-14 22:12:02 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 17. Diving into Redux: An Alternative to the Context API (Working with Multiple Slices).

--- 3. Advanced Concepts/17. Diving into Redux (An Alternative to the Context API) /react-advanced_concepts/src/App.js
+++ 3. Advanced Concepts/17. Diving into Redux (An Alternative to the Context API) /react-advanced_concepts/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Auth from './components/Auth';
 import Counter from './components/Counter';
 // import Counter from './components/Counter-ClassBasedComponent';
 
 function App() {
    return (
       <div>
-         <Counter />
+         <Fragment>
+            <Header />
+            <Auth />
+            <Counter />
+         </Fragment>
       </div>
    );
 };
