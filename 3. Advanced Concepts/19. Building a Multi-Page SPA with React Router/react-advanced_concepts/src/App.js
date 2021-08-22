3589eeaafd6bbe654eb5ebc093fe95c332c12b47
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-08-22 16:51:00 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 19. Building a Multi-Page SPA with React Router (Redirecting the User).

--- 3. Advanced Concepts/19. Building a Multi-Page SPA with React Router/react-advanced_concepts/src/App.js
+++ 3. Advanced Concepts/19. Building a Multi-Page SPA with React Router/react-advanced_concepts/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import MainHeader from './components/MainHeader';
 import Welcome from './pages/Welcome';
@@ -12,6 +12,9 @@ const App = () => {
          <MainHeader />
          <main>
             <Switch>
+               <Route exact path="/">
+                  <Redirect to="/welcome" />
+               </Route>
                <Route path="/welcome">
                   <Welcome />
                </Route>
