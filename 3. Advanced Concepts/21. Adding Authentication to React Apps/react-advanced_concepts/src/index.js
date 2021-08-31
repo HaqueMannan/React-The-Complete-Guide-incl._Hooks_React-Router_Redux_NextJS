794602cd7ba315f854df59d5a84abba06415c7bb
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-08-31 21:26:55 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 21. Adding Authentication to React Apps (Managing the Auth State with Context).

--- 3. Advanced Concepts/21. Adding Authentication to React Apps/react-advanced_concepts/src/index.js
+++ 3. Advanced Concepts/21. Adding Authentication to React Apps/react-advanced_concepts/src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 
-ReactDOM.render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(
+   <React.StrictMode>
+      <AuthContextProvider>
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
+      </AuthContextProvider>
+   </React.StrictMode>,
+   document.getElementById('root')
+);
