5851e94e2d90c2ee1177891ab64e2392aae12b46
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-08-31 20:50:10 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 21. Adding Authentication to React Apps (Adding User Signup & Login).

--- 3. Advanced Concepts/21. Adding Authentication to React Apps/react-advanced_concepts/src/components/Auth/AuthForm.js
+++ 3. Advanced Concepts/21. Adding Authentication to React Apps/react-advanced_concepts/src/components/Auth/AuthForm.js
@@ -1,28 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+   const emailInputRef = useRef();
+   const passwordInputRef = useRef();
+
    const [isLogin, setIsLogin] = useState(true);
+   const [isLoading, setIsLoading] = useState(false);
 
    const switchAuthModeHandler = () => {
       setIsLogin((prevState) => !prevState);
    };
 
+   const submitHandler = (event) => {
+      event.preventDefault();
+
+      const enteredEmail = emailInputRef.current.value;
+      const enteredPassword = passwordInputRef.current.value;
+
+      // Optional: Add Validation
+
+      setIsLoading(true);
+      let url;
+
+      if(isLogin) {
+         url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAv_ELkr2S7sOfDKPi1d9Jd39kmLFzRvHo'
+      } else {
+         url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAv_ELkr2S7sOfDKPi1d9Jd39kmLFzRvHo'
+      };
+
+      fetch(url, {
+         method: 'post',
+         body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true
+         }),
+         headers: {
+            'Content-Type': 'application/json'
+         }
+      }).then(res => {
+         setIsLoading(false);
+         if(res.ok) {
+            return res.json();
+         } else {
+            return res.json().then(data => {
+               let errorMessage = 'Authentication Failed!';
+               if(data && data.error && data.error.message) {
+                  errorMessage = data.error.message;
+               };
+               throw new Error(errorMessage);
+            });
+         };
+      }).then(data => {
+         console.log(data);
+      }).catch(err => {
+         alert(err.message);
+      });
+   };
+
    return (
       <section className={classes.auth}>
          <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-         <form>
+         <form onSubmit={submitHandler}>
             <div className={classes.control}>
                <label htmlFor='email'>Your Email</label>
-               <input type='email' id='email' required />
+               <input type='email' id='email' required ref={emailInputRef} />
             </div>
             <div className={classes.control}>
                <label htmlFor='password'>Your Password</label>
-               <input type='password' id='password' required />
+               <input type='password' id='password' required ref={passwordInputRef} />
             </div>
             <div className={classes.actions}>
-               <button>{isLogin ? 'Login' : 'Create Account'}</button>
+               {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+               {isLoading && <p>Sending Request...</p>}
                <button type='button' className={classes.toggle} onClick={switchAuthModeHandler}>
                   {isLogin ? 'Create new account' : 'Login with existing account'}
                </button>
