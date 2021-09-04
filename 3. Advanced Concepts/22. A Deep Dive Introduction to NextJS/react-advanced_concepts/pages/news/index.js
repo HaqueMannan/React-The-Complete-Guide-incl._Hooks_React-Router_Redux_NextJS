ff9608ad09f7b08e447e379c2a0cef698c039131
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-09-04 01:43:22 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 22. A Deep Dive Introduction to Next.js (Linking Between Pages).

--- 3. Advanced Concepts/22. A Deep Dive Introduction to NextJS/react-advanced_concepts/pages/news/index.js
+++ 3. Advanced Concepts/22. A Deep Dive Introduction to NextJS/react-advanced_concepts/pages/news/index.js
@@ -1,6 +1,15 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 function NewsPage() {
    return(
-      <h1>The News Page</h1>
+      <Fragment>
+         <h1>The News Page</h1>
+         <ul>
+            <li><Link href="news/nextjs-is-a-framework">NextJS is a Framework</Link></li>
+            <li><Link href="news/reactjs-is-a-library">React JS is a Library</Link></li>
+         </ul>
+      </Fragment>
    );
 };
 
