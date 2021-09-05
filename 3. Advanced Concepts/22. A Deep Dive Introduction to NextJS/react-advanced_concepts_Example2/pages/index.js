5fdc54de9a4b1bab8cbac590d098125eab22e195
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-09-05 14:07:55 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 22. A Deep Dive Introduction to Next.js (Sending HTTP Requests to API Routes & Fetching Data From a Database).

--- 3. Advanced Concepts/22. A Deep Dive Introduction to NextJS/react-advanced_concepts_Example2/pages/index.js
+++ 3. Advanced Concepts/22. A Deep Dive Introduction to NextJS/react-advanced_concepts_Example2/pages/index.js
@@ -1,9 +1,11 @@
+import { MongoClient } from 'mongodb';
+
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-   { id: 'm1', title: 'A First Meetup', image: 'https://media.cntraveler.com/photos/5fc6818f3cfe1de2cab79372/4:3/w_4000,h_3000,c_limit/Amsterdam-GettyImages-840603854.jpg', address: 'Boat Tour in Amsterdam', description: 'This is a first meetup'},
-   { id: 'm2', title: 'A Second Meetup', image: 'https://i0.wp.com/i.pinimg.com/originals/e3/b5/4d/e3b54de6c65ae3b90ac640126d586ed0.jpg', address: 'Coffee Shop in Italy', description: 'This is a second meetup'}
-];
+// const DUMMY_MEETUPS = [
+//    { id: 'm1', title: 'A First Meetup', image: 'https://media.cntraveler.com/photos/5fc6818f3cfe1de2cab79372/4:3/w_4000,h_3000,c_limit/Amsterdam-GettyImages-840603854.jpg', address: 'Boat Tour in Amsterdam', description: 'This is a first meetup'},
+//    { id: 'm2', title: 'A Second Meetup', image: 'https://i0.wp.com/i.pinimg.com/originals/e3/b5/4d/e3b54de6c65ae3b90ac640126d586ed0.jpg', address: 'Coffee Shop in Italy', description: 'This is a second meetup'}
+// ];
 
 function HomePage(props) {
    return(
@@ -14,9 +16,21 @@ function HomePage(props) {
 // Static Site Generation (SSG):
 export async function getStaticProps(){
    // fetch data from an API
+   const client = await MongoClient.connect('mongodb+srv://Tester:OXIsMht4XrKqbcpz@cluster0.evwfu.mongodb.net/meetupsDB?retryWrites=true&w=majority');
+   const db = client.db();
+   const meetupsCollection = db.collection('meetups');
+   const meetups = await meetupsCollection.find().toArray();
+   client.close();
+
    return {
       props: {
-         meetups: DUMMY_MEETUPS
+         // meetups: DUMMY_MEETUPS
+         meetups: meetups.map(meetup => ({
+            id: meetup._id.toString(),
+            title: meetup.title,
+            address: meetup.address,
+            image: meetup.image,
+         }))
       },
       revalidate: 10    // Re-validate the SSG every 10 seconds
    };
