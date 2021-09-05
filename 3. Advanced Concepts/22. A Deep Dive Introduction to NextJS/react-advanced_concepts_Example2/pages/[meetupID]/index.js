70f23d05b61f1a0263de583264b7b7271f059102
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-09-05 01:24:00 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 22. A Deep Dive Introduction to Next.js (Working with Dynamic Params for SSG Data Fetching).

--- 3. Advanced Concepts/22. A Deep Dive Introduction to NextJS/react-advanced_concepts_Example2/pages/[meetupID]/index.js
+++ 3. Advanced Concepts/22. A Deep Dive Introduction to NextJS/react-advanced_concepts_Example2/pages/[meetupID]/index.js
@@ -11,4 +11,32 @@ function MeetupDetails() {
    );
 };
 
+export async function getStaticPaths() {
+   return {
+      fallback: false,
+      paths: [
+         { params: { meetupID: 'm1'} },
+         { params: { meetupID: 'm2'} }
+      ]
+   }
+};
+
+export async function getStaticProps(context) {
+   const meetupId = context.params.meetupID;
+   console.log(meetupId);
+
+   // fetch data for a single meetup
+   return {
+      props: {
+         meetupData: {
+            id: meetupId,
+            image: 'https://media.cntraveler.com/photos/5fc6818f3cfe1de2cab79372/4:3/w_4000,h_3000,c_limit/Amsterdam-GettyImages-840603854.jpg',
+            title: 'First Meetup',
+            address: '1 Fake Road, Fake City',
+            description: 'This is a first meetup'
+         }
+      }
+   };
+};
+
 export default MeetupDetails;
