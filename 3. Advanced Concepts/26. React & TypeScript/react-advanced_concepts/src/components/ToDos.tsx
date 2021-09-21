140e1282c7b89179924f790c964a0bcd887eec3a
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-09-21 23:11:34 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 26. React & TypeScript (Working with Components/Props & TypeScript).

---
+++ 3. Advanced Concepts/26. React & TypeScript/react-advanced_concepts/src/components/ToDos.tsx
@@ -0,0 +1,9 @@
+const ToDos: React.FC<{items: string[]}> = (props) => {
+   return(
+      <ul>
+         {props.items.map(item => <li key={item}>{item}</li>)}
+      </ul>
+   );
+};
+
+export default ToDos;
