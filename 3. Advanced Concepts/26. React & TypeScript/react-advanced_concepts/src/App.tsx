dfb1b015dc90825699fd73f6aa20938ed78a0770
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-09-24 21:10:44 +0100

    React The Complete Guide (incl. Hooks, React Router, Redux, NextJS): Advanced Concepts - 26. React & TypeScript (Adding a Data Model).

--- 3. Advanced Concepts/26. React & TypeScript/react-advanced_concepts/src/App.tsx
+++ 3. Advanced Concepts/26. React & TypeScript/react-advanced_concepts/src/App.tsx
@@ -1,9 +1,15 @@
 import ToDos from './components/ToDos';
+import Todo from './models/todo';
 
 function App() {
+   const todos = [
+      new Todo('Learn React'),
+      new Todo('Learn TypeScript')
+   ];
+
    return (
       <div>
-         <ToDos items={['Learn React', 'Learn TypeScript']} />
+         <ToDos items={todos} />
       </div>
    );
 };
--- 3. Advanced Concepts/26. React & TypeScript/react-advanced_concepts/src/components/ToDos.tsx
+++ 3. Advanced Concepts/26. React & TypeScript/react-advanced_concepts/src/components/ToDos.tsx
@@ -1,7 +1,11 @@
-const ToDos: React.FC<{items: string[]}> = (props) => {
+import Todo from '../models/todo';
+
+const ToDos: React.FC<{items: Todo[]}> = (props) => {
    return(
       <ul>
-         {props.items.map(item => <li key={item}>{item}</li>)}
+         {props.items.map((item) => (
+            <li key={item.id}>{item.text}</li>)
+         )}
       </ul>
    );
 };
